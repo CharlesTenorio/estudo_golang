c2afd31e28b5e8e57ab8dc764979d5cdd0168d1d
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2019-12-01 08:38:01 -0300

    composicao

---
+++ pheranca.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type endereco struct {
+	cep    string
+	rua    string
+	bairro string
+	cidade string
+}
+
+type pessoa struct {
+	nome  string
+	idade int32
+	endereco
+}
+
+func main() {
+	p := pessoa{}
+	p.nome = "Charles"
+	p.idade = 46
+	p.bairro = "centro"
+
+	p2 := pessoa{}
+	p2.nome = "Kyara"
+	p2.idade = 5
+	p2.bairro = "sao jose"
+
+	fmt.Println(p.nome, p.bairro)
+	fmt.Println(p2.nome, p2.bairro)
+}
